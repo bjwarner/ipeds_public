6ddad728b1b2a22adfab4e5ca9b45f5843e5af19
Bijan Warner <bijanwarner@gmail.com>
2017-07-04 19:46:39 -0500

    Added function to read in spss syntax files

--- ipeds_build.R
+++ ipeds_build.R
@@ -13,7 +13,10 @@
 # Directory, grad rate, GASB/FASB finance files
 
 filelist <-c("EFFY2012","EFFY2013","EFFY2014","EFFY2015",
-             "HD2015")
+             "IC2012","IC2013","ADM2014","ADM2015",
+             "EF2012A","EF2013A","EF2014A","EF2015A",
+             "EF2012B","EF2013B","EF2014B","EF2015B")
+
 
 
 # Use list apply to run through list of file names,
@@ -26,14 +29,44 @@ lapply(filelist, function(ipedfile){
   temp <-tempfile()
   download.file(urltemp,temp)
   tempdata <- read.csv(unz(temp,paste0(tolower(ipedfile),".csv")))
-  write.csv(tempdata,paste0(tolower(ipedfile),".csv"))
+  
+  #revise location path (create first)
+  write.csv(tempdata,paste0("./data/",tolower(ipedfile),".csv"))
+  
   ##check if revised file exists
   if (paste0(tolower(ipedfile),"_rv.csv") %in% unzip(temp,list=TRUE)$Name) {
     tempdata_rv <- read.csv(unz(temp,paste0(tolower(ipedfile),"_rv.csv")))
-    write.csv(tempdata_rv,paste0(tolower(ipedfile),"_rv.csv"))
+    write.csv(tempdata_rv,paste0("./data/",tolower(ipedfile),"_rv.csv"))
   }
   unlink(temp)
 })
+
+
+# Now download .sps files for variable names and values.
+#Convention is to append "_SPS.zip" for read in files.
+#filelist_sps <- lapply(filelist,paste0,"_SPS")
+
+lapply(filelist, function(spsfile){
+  urltemp <- paste0("https://nces.ed.gov/ipeds/datacenter/data/",
+                    spsfile,
+                    "_SPS.zip")
+  temp <-tempfile()
+  download.file(urltemp,temp)
+  fileConn<-unz(temp,paste0(tolower(spsfile),".sps"))
+  text_sps <- readChar(fileConn,nchars=1000000)
+  
+  #note, need to eliminate duplicate line returns "\r\n"
+  text_sps <- gsub("\r\n","\r",text_sps)
+  
+  #create syntax sub-folder before running
+  savConn<-file(paste0("./syntax/",tolower(spsfile),".sps"))
+  writeLines(text_sps,savConn)
+  close(fileConn)
+  close(savConn)
+  unlink(temp)
+}
+)
+
 
 
 # END ####
